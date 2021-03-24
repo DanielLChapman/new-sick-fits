947868e51b559be44ffa53743b995378d1b93551
DanielLChapman <danielc@my.ccsu.edu>
2021-03-24 17:23:53 -0400

    Keystone and database set up

---
+++ backend/schemas/ProductImage.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { relationship, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+import { cloudinaryImage } from '@keystone-next/cloudinary';
+import { isSignedIn, permissions } from '../access';
+
+export const cloudinary = {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.CLOUDINARY_KEY,
+    apiSecret: process.env.CLOUDINARY_SECRET,
+    folder: 'sick-fits-2',
+};
+
+export const ProductImage = list({
+    fields: {
+        image: cloudinaryImage({
+            cloudinary,
+            label: 'Source',
+        }),
+        altText: text(),
+        product: relationship({ ref: 'Product.photo' }),
+    },
+    ui: {
+        listView: {
+            initialColumns: ['image', 'altText', 'product'],
+        },
+    },
+});
