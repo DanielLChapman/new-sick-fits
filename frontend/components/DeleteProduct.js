c4a8cbb1f055fb6ac309b01935a317aa97a98127
DanielLChapman <danielc@my.ccsu.edu>
2021-03-29 15:25:58 -0400

    delete button

---
+++ frontend/components/DeleteProduct.js
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import { useMutation } from '@apollo/client';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function DeleteProduct({ id, children }) {
+  const [deleteProduct, { data, error, loading }] = useMutation(
+    DELETE_PRODUCT_MUTATION,
+    {
+      variables: { id },
+    }
+  );
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          // go ahead and delete it
+          console.log('DELETING');
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+DeleteProduct.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.any,
+};
+
+export default DeleteProduct;
