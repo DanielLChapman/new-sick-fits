196e2b00fdbb779ef866970965203f0b8fa86838
DanielLChapman <danielc@my.ccsu.edu>
2021-03-31 15:20:23 -0400

    Dynamic routing

---
+++ frontend/pages/products/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+export default function IndexPage() {
+  const router = useRouter();
+  const page = parseInt(router.query.page, 10) || 1;
+  return (
+    <>
+      <Pagination page={page} />
+      <Products />;
+      <Pagination page={page} />
+    </>
+  );
+}
