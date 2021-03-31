d440a2a2c561712c99b22df997750c44db770d98
DanielLChapman <danielc@my.ccsu.edu>
2021-03-31 17:27:12 -0400

    Query current user

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -28,6 +28,7 @@ function DeleteProduct({ id, children }) {
       type="button"
       disabled={loading}
       onClick={() => {
+        // eslint-disable-next-line no-restricted-globals
         if (confirm('Are you sure you want to delete this item?')) {
           // go ahead and delete it
           console.log('DELETING');
---
+++ frontend/pages/signin.js
@@ -0,0 +1,3 @@
+export default function SignInPage() {
+  return <p>Hello!</p>;
+}
