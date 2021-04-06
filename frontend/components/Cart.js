1f6b10745acf72b574f8212af9bd4a488e438634
DanielLChapman <danielc@my.ccsu.edu>
2021-04-05 22:24:25 -0400

    Remove from Cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -7,6 +7,7 @@ import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/cartState';
 import CloseButton from './styles/CloseButton';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -40,6 +41,7 @@ function CartItem({ cartItem }) {
             {cartItem.quantity} &times; {formatMoney(product.price)} each
           </em>
         </p>
+        <RemoveFromCart id={cartItem.id} />
       </div>
     </CartItemStyles>
   );
