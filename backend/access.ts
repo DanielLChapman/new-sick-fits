25d289863f8772c74ee9b567576338321ffca448
DanielLChapman <danielc@my.ccsu.edu>
2021-04-23 15:21:06 -0400

    Permission access functions

--- backend/access.ts
+++ backend/access.ts
@@ -1,7 +1,26 @@
 // at its simplest, access control is either a yes or no value
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
     return !!sessionStorage;
 }
+
+const generatedPermissions = Object.fromEntries(
+    permissionsList.map((permission) => [
+        permission,
+        function ({ session }: ListAccessArgs) {
+            return !!session?.data.role?.[permission];
+        },
+    ])
+);
+
+
+// Permissions check if someone meets a criteria - yes or no.
+export const permissions = {
+    ...generatedPermissions,
+    isAwesome({ session }: ListAccessArgs): boolean {
+        return session?.data.name.includes('wes');
+    },
+};
